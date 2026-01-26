8ef0cc6f18cc3ebe1486b02ad706c007a6cb57c1
escuSerban <serbanghermanescu58@gmail.com>
2026-01-26 22:39:51 +0200

    Created the API layer

---
+++ feature_weather/src/main/java/com/example/weatherpulse/feature_weather/data/remote/WeatherApi.kt
@@ -0,0 +1,25 @@
+package com.example.weatherpulse.feature_weather.data.remote
+
+import com.example.weatherpulse.feature_weather.data.remote.dto.GeoDirectDto
+import com.example.weatherpulse.feature_weather.data.remote.dto.OneCallDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("data/3.0/onecall")
+    suspend fun getWeatherData(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") units: String = "metric",
+        @Query("appid") apiKey: String
+    ): OneCallDto
+
+    @GET("geo/1.0/direct")
+    suspend fun getCoordinates(
+        @Query("q") cityName: String,
+        @Query("limit") limit: Int = 1,
+        @Query("appid") apiKey: String
+    ): List<GeoDirectDto>
+
+}
