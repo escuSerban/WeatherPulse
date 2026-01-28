c777af71ce83a60c4241e868138bdde1bd317535
escuSerban <serbanghermanescu58@gmail.com>
2026-01-28 20:30:36 +0200

    Added logic to hide keyboard after we fetched the data

--- feature_weather/src/main/java/com/example/weatherpulse/feature_weather/presentation/ui/WeatherScreen.kt
+++ feature_weather/src/main/java/com/example/weatherpulse/feature_weather/presentation/ui/WeatherScreen.kt
@@ -1,14 +1,30 @@
 package com.example.weatherpulse.feature_weather.presentation.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.weatherpulse.feature_weather.domain.model.WeatherForecast
@@ -21,6 +37,15 @@ fun WeatherScreen(
     viewModel: WeatherViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsState()
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    // This effect will run when `state.isLoading` changes.
+    // When loading finishes (isLoading becomes false), it hides the keyboard.
+    LaunchedEffect(state.isLoading) {
+        if (!state.isLoading) {
+            keyboardController?.hide()
+        }
+    }
 
     Column(
         modifier = modifier
